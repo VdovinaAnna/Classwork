f7a05738b47200d655d94f6f492a9505472c841c
Anna <vdovinanna1991@gmail.com>
2022-11-10 15:14:48 +0300

    Programm

--- Finish_Homework/FinishProject/Program.cs
+++ Finish_Homework/FinishProject/Program.cs
@@ -1,5 +1,27 @@
 ﻿string[] symbols1 = new string[5] {"111", "1", "hello", "world", ";)"};
 string[] symbols2 = new string[symbols1.Length];
 
+void FillArray(string[] symbols1, string[] symbols2)
+{
+    int count = 0;
+    for (int i = 0; i < symbols1.Length; i++)
+    {
+    if(symbols1[i].Length <= 3)
+        {
+        symbols2[count] = symbols1[i];
+        count++;
+        }
+}
+}
+
+void PrintArray(string[] symbols)
+{
+    for (int i = 0; i < symbols.Length; i++)
+    {
+       Console.Write($"{symbols[i]} ");
+    }
+    Console.WriteLine();
+}
+
 FillArray(symbols1, symbols2);
 PrintArray(symbols2);
